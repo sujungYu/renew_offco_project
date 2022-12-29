6589778ee6eee8a21f1270b3db53bdd83804b838
sujungYu <su_moment@naver.com>
2022-12-29 13:41:06 +0900

    refactor(LoginForm) : 변수명 및 스타일 수정

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,25 +1,17 @@
 <template>
   <div>
-    <form @submit.prevent="submitForm">
-      <!-- <h2>로그인 유지<input type="checkbox" v-model="setlogin" /></h2> -->
-      <div>
-        <input class="form" type="text" placeholder="아이디" v-model="userid" />
-      </div>
-      <div>
-        <input
-          class="form"
-          type="password"
-          placeholder="비밀번호"
-          v-model="userpassword"
-          @keyup.enter="submitForm()"
-        />
-      </div>
-      <button class="login-button" type="submit">
-        LOGIN
-      </button>
+    <form @submit.prevent="submitLoginForm">
+      <input class="id" type="text" placeholder="아이디" v-model="userId" />
+      <input
+        class="password"
+        type="password"
+        placeholder="비밀번호"
+        v-model="userPassword"
+        @keyup.enter="submitLoginForm()"
+      />
+      <button class="login-button">LOGIN</button>
     </form>
-    <!-- <h3><router-link to="/findid">아이디 찾기</router-link>&nbsp;&nbsp;&nbsp; | <router-link to="/findpw">&nbsp;&nbsp;&nbsp;비밀번호 찾기</router-link></h3> -->
-    <button class="signup-button" @click="signUp">회원가입</button>
+    <button class="signup-button" @click="moveSignupPage">회원가입</button>
   </div>
 </template>
 
@@ -27,23 +19,20 @@
 export default {
   data() {
     return {
-      setlogin: false,
-      userid: '',
-      userpassword: '',
+      userId: '',
+      userPassword: '',
     };
   },
-  created() {
-    // const userid = localStorage.getItem('userid');
-    // if (userid) {
-    // }
-  },
   methods: {
-    async submitForm() {
-      await this.$store.dispatch('login', this.userid);
-      const userId = this.$store.state.LoginSignup.nowUserId;
-      this.$router.push(`/home/${userId}`);
+    async submitLoginForm() {
+      await this.$store.dispatch('login', this.userId);
+      const userNumber = this.$store.state.LoginSignup.nowUserId;
+      this.moveHomePage(userNumber);
     },
-    signUp() {
+    moveHomePage(userNumber) {
+      this.$router.push(`/home/${userNumber}`);
+    },
+    moveSignupPage() {
       this.$router.replace('./signup');
     },
   },
@@ -51,18 +40,8 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Gothic+A1&display=swap');
-h2 {
-  margin-left: auto;
-  margin-right: 2em;
-  font-weight: lighter;
-  font-family: 'Gothic A1', sans-serif;
-  font-size: 1.3em;
-  width: 28.1vw;
-  height: 2.63vh;
-  margin-top: 2.31em;
-}
-.form {
+.id,
+.password {
   display: inline-block;
   width: 82vw;
   height: 6.1vh;
@@ -71,27 +50,24 @@ h2 {
   border: 1px solid rgba(162, 210, 255, 100);
   border-radius: 15px;
 }
-.login-button {
-  font-family: 'Gothic A1', sans-serif;
-  font-size: 2.6em;
-  width: 82vw;
-  height: 9vh;
-  border: rgb(28, 31, 34);
-  border-radius: 15px;
-  background-color: rgb(162, 210, 255);
-  color: rgb(57, 62, 70);
-  margin-top: 0.8em;
-  margin-bottom: 0.8em;
-}
+.login-button,
 .signup-button {
   font-family: 'Gothic A1', sans-serif;
   font-size: 2.6em;
   width: 82vw;
   height: 9vh;
-  border: 1px solid rgb(255, 134, 94);
+  margin-top: 0.8em;
   border-radius: 15px;
+}
+.login-button {
+  border: rgb(28, 31, 34);
+  background-color: rgb(162, 210, 255);
+  color: rgb(57, 62, 70);
+  margin-bottom: 0.8em;
+}
+.signup-button {
+  border: 1px solid rgb(255, 134, 94);
   background-color: white;
   color: rgb(255, 134, 94);
-  margin-top: 0.8em;
 }
 </style>
