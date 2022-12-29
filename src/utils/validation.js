d9f7663b113f5ef597db5443b55a4bd19526772b
sujungYu <su_moment@naver.com>
2022-12-29 14:36:34 +0900

    feat(validation) : 비밀번호, 중복확인 유효성 검사 기능

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,6 +1,16 @@
 function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
 }
 
-export { validateEmail };
+function validatePassword(password) {
+  let regex = /^[0-9]+$/;
+  return regex.test(Number(password));
+}
+
+function validateCheckPassword(password, checkPassword) {
+  if (password == checkPassword) return true;
+  return false;
+}
+
+export { validateEmail, validatePassword, validateCheckPassword };
