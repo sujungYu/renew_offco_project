425f5dcd4696e97d4c962d7ed5b302c5ea7c464a
sujungYu <su_moment@naver.com>
2022-12-29 15:03:58 +0900

    refactor(AcceptRoomPage) : 스타일 수정

--- src/views/AcceptRoomPage.vue
+++ src/views/AcceptRoomPage.vue
@@ -4,7 +4,7 @@
       <i class="far fa-check-circle yes"></i>
     </h1>
     <h2>초대를 수락하시겠습니까?</h2>
-    <button @click="accept">OK</button>
+    <button @click="acceptInvite">OK</button>
   </div>
 </template>
 
@@ -18,13 +18,11 @@ export default {
   created() {
     this.token = this.$route.params.token;
     this.$store.dispatch('checkToken', this.token);
-    console.log(this.$store.state.LoginSignup.nowUserName);
   },
   methods: {
-    async accept() {
+    async acceptInvite() {
       await this.$store.dispatch('RoomMember', this.$store.getters.TokenId);
       alert('invitation succeed');
-      console.log(this.$store.state.LoginSignup.nowUserId);
       this.$router.push(`/roomhome/${this.$store.getters.TokenId}`);
     },
   },
