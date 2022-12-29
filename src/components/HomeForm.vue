e8bfd2a530389d4dee951bc82bb286b2fea22e86
sujungYu <su_moment@naver.com>
2022-12-29 14:13:48 +0900

    refactor(HomeForm) : 변수명 수정

--- src/components/HomeForm.vue
+++ src/components/HomeForm.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-    <ul>
-      <li v-for="room in roomList" :key="room.roomName">
+    <ul class="home-container">
+      <li v-for="room in rooms" :key="room.roomName">
         <router-link
           :to="/roomhome/ + room.roomId"
           tag="span"
           style="cursor: pointer"
         >
-          <div class="box">{{ room.roomName }}</div>
+          <div class="room-name">{{ room.roomName }}</div>
         </router-link>
-        <hr
-          style="width:81vw; height:1px; border:none; background-color:rgba(57, 62, 70, 0.4); margin:2.8vh 0"
-        />
+        <hr class="line" />
       </li>
     </ul>
-    <i class="fas fa-plus-circle createroom" @click="createRoom"></i>
-    <modal v-show="showModal" :open="showModal" @close="showModal = false">
+    <i class="fas fa-plus-circle create-room" @click="createNewRoom"></i>
+    <modal
+      v-show="createRoomModal"
+      :open="createRoomModal"
+      @close="createRoomModal = false"
+    >
       <room-create></room-create>
     </modal>
   </div>
@@ -31,39 +33,35 @@ export default {
   },
   data() {
     return {
-      roomList: [],
-      showModal: false,
+      rooms: [],
+      createRoomModal: false,
     };
   },
-  async created() {
-    // this.$store.commit('initData');
-    await this.$store.dispatch('myRoomList', this.$route.params.id);
-    this.roomList = this.$store.state.Room.myRooms;
+  created() {
+    this.getRoomSet();
   },
   methods: {
-    createRoom() {
-      this.showModal = !this.showModal;
+    async getRoomSet() {
+      await this.$store.dispatch('myRoomList', this.$route.params.id);
+      this.rooms = this.$store.state.Room.myRooms;
+    },
+    createNewRoom() {
+      this.createRoomModal = !this.createRoomModal;
     },
   },
 };
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Gothic+A1&display=swap');
-/* div {
-  margin: 0 auto;
-  font-size: 10px;
-  text-align: center;
-} */
-ul {
+.home-container {
   padding-left: 0;
   list-style-type: none;
   text-align: center;
 }
-li {
+.home-container li {
   display: inline-block;
 }
-.box {
+.room-name {
   width: 80.9vw;
   height: 8.3vh;
   font-family: 'Gothic A1', sans-serif;
@@ -74,11 +72,18 @@ li {
   margin-bottom: 2.1vh;
   background-color: white;
 }
-.createroom {
+.create-room {
   font-size: 3.5em;
   color: rgb(255, 134, 94);
   left: 32vw;
   position: relative;
   margin-bottom: 3vh;
+}
+.line {
+  width: 81vw;
+  height: 1px;
+  border: none;
+  background-color: rgba(57, 62, 70, 0.4);
+  margin: 2.8vh 0;
 }
 </style>
